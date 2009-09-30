81f35d647dd5ce0accb403989ac02e9999ba5121
newcreat <newcreat@libnge.org>
2009-09-30 03:31:34 +0000

    由于先前svn丢失，版本号丢失，所以版本号重新从0开始...

--- src/nge_debug_log.c
+++ src/nge_debug_log.c
@@ -1,39 +1,4 @@
-#include <windows.h>
 #include "nge_debug_log.h"
-
-CONSOLE_SCREEN_BUFFER_INFO   csbiInfo; 
-HANDLE   hStdout;
-WORD   SetRedText()//切换成红色显示 
-{   
-	unsigned short   wOldColorAttrs;   
-	hStdout=GetStdHandle(STD_OUTPUT_HANDLE);//控制台窗口句柄   
-  
-
-//hStdout   =   GetStdHandle(STD_OUTPUT_HANDLE);     
-//   Save   the   current   text   colors.     
-if   (!   GetConsoleScreenBufferInfo(hStdout,   &csbiInfo))     
-{  // MessageBox(NULL,   "GetConsoleScreenBufferInfo",   "Console   Error",   MB_OK);     
-return   0;   
-}   
-wOldColorAttrs   =   csbiInfo.wAttributes;     
-if(!SetConsoleTextAttribute(hStdout,   
-   FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY | BACKGROUND_BLUE))   
-{   //MessageBox(NULL,   "SetConsoleTextAttribute",   "Console   Error",   MB_OK);   
-return   0;   
-}   
-return   wOldColorAttrs;   
-//     SetConsoleTextAttribute(hStdout,   wOldColorAttrs);   
-}   
-void   RestoreText(WORD   wOldColorAttrs)   
-{   //HANDLE   hStdout;   
-	//hStdout   =   GetStdHandle(STD_OUTPUT_HANDLE);   
-	SetConsoleTextAttribute(hStdout,   wOldColorAttrs);   
-}
-
-
-
-
-
 
 #ifdef _DEBUG_LOG
 
@@ -53,9 +18,8 @@ void nge_log (const char* pMessage, ...)
 	char  FileInfo[256] = {0};
 	va_list	ArgPtr;
 	FILE*	pFile;
-	
 	static char FirstLog[128]={"-------------------------=NGE LOG=-------------------------\n"};
-	SetRedText();
+	
 	if(first_run == 0){
 		pFile = fopen ("nge_log.txt", "a+b");
 		fwrite (FirstLog, strlen (FirstLog), 1, pFile);
