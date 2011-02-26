f9da7b3a2e4b1cb637024feaa8ddb86db3dc6d7c
Kun Wang <ifreedom.cn@gmail.com>
2011-02-26 23:24:01 +0800

    Fix build error on psp-gcc-4.3.5 (minpspw).
    build error : invalid conversion from 'uint32_t*' to 'long unsigned int*'

--- coolaudio/codecs/mp3_codec.cpp
+++ coolaudio/codecs/mp3_codec.cpp
@@ -1,28 +1,28 @@
-/* 
- *	 _____  _                                 _____ 
+/*
+ *	 _____  _                                 _____
  *	/  ___|(_)                               / __  \
  *	\ `--.  _  _ __  ___  _ __   ___  __   __`' / /'
- *	 `--. \| || '__|/ _ \| '_ \ / __| \ \ / /  / /  
+ *	 `--. \| || '__|/ _ \| '_ \ / __| \ \ / /  / /
  *	/\__/ /| || |  |  __/| | | |\__ \  \ V / ./ /___
  *	\____/ |_||_|   \___||_| |_||___/   \_/  \_____/
- *	
- *	
+ *
+ *
  *	Copyright (C) 2007 cooleyes
- *	eyes.cooleyes@gmail.com 
+ *	eyes.cooleyes@gmail.com
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2, or (at your option)
  *  any later version.
- *   
+ *
  *  This Program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU General Public License for more details.
- *   
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with GNU Make; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *  http://www.gnu.org/copyleft/gpl.html
  *
  */
@@ -42,14 +42,14 @@ char* Mp3Codec::initialize(AudioCodecInitData* init_data) {
 	audio_samplerate = init_data->samplerate;
 	audio_samples_per_frame = init_data->samples_per_frame;
 	audio_type = 0x1002;
-	
+
 	memset(me_audio_codec_buffer, 0, 65*sizeof(uint32_t));
-	if ( sceAudiocodecCheckNeedMem(me_audio_codec_buffer, audio_type) < 0 ) 
+	if ( sceAudiocodecCheckNeedMem((long unsigned int*) me_audio_codec_buffer, audio_type) < 0 )
 		return "Mp3Codec : sceAudiocodecCheckNeedMem fail";
-	if ( sceAudiocodecGetEDRAM(me_audio_codec_buffer, audio_type) < 0 )
+	if ( sceAudiocodecGetEDRAM((long unsigned int*)me_audio_codec_buffer, audio_type) < 0 )
 		return "Mp3Codec : sceAudiocodecGetEDRAM fail";
-	if ( sceAudiocodecInit(me_audio_codec_buffer, audio_type) < 0 ) {
-		sceAudiocodecReleaseEDRAM(me_audio_codec_buffer);
+	if ( sceAudiocodecInit((long unsigned int*)me_audio_codec_buffer, audio_type) < 0 ) {
+		sceAudiocodecReleaseEDRAM((long unsigned int*)me_audio_codec_buffer);
 		return "Mp3Codec : sceAudiocodecInit fail";
 	}
 	init_flag = true;
@@ -58,7 +58,7 @@ char* Mp3Codec::initialize(AudioCodecInitData* init_data) {
 
 void Mp3Codec::finalize() {
 	if( init_flag ) {
-		sceAudiocodecReleaseEDRAM(me_audio_codec_buffer);
+		sceAudiocodecReleaseEDRAM((long unsigned int*)me_audio_codec_buffer);
 		init_flag = false;
 	}
 };
@@ -70,11 +70,10 @@ int32_t Mp3Codec::decode(void* data, uint32_t data_len, void* pcm_data) {
 		me_audio_codec_buffer[7] = data_len;
 		me_audio_codec_buffer[10] = data_len;
 		me_audio_codec_buffer[9] = audio_samples_per_frame << 2;
-		if ( sceAudiocodecDecode(me_audio_codec_buffer, audio_type) < 0 )
+		if ( sceAudiocodecDecode((long unsigned int*)me_audio_codec_buffer, audio_type) < 0 )
 			;//return 0;
 		return audio_samples_per_frame;
 	}
 	else
 		return -1;
 };
-
