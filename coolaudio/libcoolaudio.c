ee499a0ebf108fc68d1b37473d8a7afbf5d3e7ad
Kun Wang <ifreedom.cn@gmail.com>
2011-02-27 09:19:34 +0800

    Add Linux and Win32 port for coolaudio(use SDL_mixer).
    Write CMakeLists.txt for cmake build.

--- coolaudio/libcoolaudio.c
+++ coolaudio/libcoolaudio.c
@@ -1,52 +1,75 @@
-#include "audio_interface.h"
-
-#ifdef WIN32
 #include <stdio.h>
 #include <stdlib.h>
-#include "SDL.h"
-//for Windows
-//default init for windows ,load SDL mixer
+#include "audio_interface.h"
+
+#if defined(WIN32) || defined(__linux__)
+#include "SDL/SDL.h"
+#include "SDL/SDL_mixer.h"
+
+//for Windows and linux
+//default init for windows and linux,load SDL mixer
 void CoolAudioDefaultInit()
 {
-	/* Initialize the SDL library */
-	if ( SDL_Init(SDL_INIT_AUDIO) < 0 ) {
-		fprintf(stderr, "Couldn't initialize SDL: %s\n",SDL_GetError());
-		exit(0);
-	}
+  /* Initialize the SDL and SDL_mixer library */
+  if ( SDL_Init(SDL_INIT_AUDIO) < 0 && Mix_Init(MIX_INIT_MP3|MIX_INIT_OGG) != (MIX_INIT_MP3|MIX_INIT_OGG) ) {
+	fprintf(stderr, "Couldn't initialize SDL: %s\n",SDL_GetError());
+	exit(1);
+  }
+
+  int rate = MIX_DEFAULT_FREQUENCY;
+  Uint16 format = MIX_DEFAULT_FORMAT;
+  int channels = MIX_DEFAULT_CHANNELS;
+
+  /* Open the audio device */
+  if (Mix_OpenAudio(rate, format, channels, 4096) < 0) {
+	fprintf(stderr, "Couldn't open audio: %s\n", Mix_GetError());
+	exit(1);
+  } else {
+	Mix_QuerySpec(&rate, &format, &channels);
+	// printf("Opened audio at %d Hz %d bit %s (%s), %d bytes audio buffer\n", rate,
+	//   (format&0xFF),
+	//   (channels > 2) ? "surround" : (channels > 1) ? "stereo" : "mono",
+	//   (format&0x1000) ? "BE" : "LE",
+	//   bufsize );
+  }
+  /* Set the music volume */
+  Mix_VolumeMusic(MIX_MAX_VOLUME);
+
+  /* Set the external music player, if any */
+  // Mix_SetMusicCMD(getenv("MUSIC_CMD"));
 }
 
 void CoolAudioDefaultFini()
 {
-	
+  Mix_CloseAudio();
+  Mix_Quit();
 }
 
 #else
 #include <pspsdk.h>
 #include <pspkernel.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <pspaudiocodec.h> 
+#include <pspaudiocodec.h>
 #include <pspmpeg.h>
 //for PSP
 //default init for psp ,load prxs
 void CoolAudioDefaultInit()
 {
 #ifdef PSP_FW_VERSION_1XX
-	int result = pspSdkLoadStartModule("flash0:/kd/me_for_vsh.prx", PSP_MEMORY_PARTITION_KERNEL); 
-    	result = pspSdkLoadStartModule("flash0:/kd/videocodec.prx", PSP_MEMORY_PARTITION_KERNEL); 
-    	result = pspSdkLoadStartModule("flash0:/kd/audiocodec.prx", PSP_MEMORY_PARTITION_KERNEL); 
-    	result = pspSdkLoadStartModule("flash0:/kd/mpegbase.prx", PSP_MEMORY_PARTITION_KERNEL); 
-    	result = pspSdkLoadStartModule("flash0:/kd/mpeg_vsh.prx", PSP_MEMORY_PARTITION_USER); 
-    	pspSdkFixupImports(result); 
+	int result = pspSdkLoadStartModule("flash0:/kd/me_for_vsh.prx", PSP_MEMORY_PARTITION_KERNEL);
+    	result = pspSdkLoadStartModule("flash0:/kd/videocodec.prx", PSP_MEMORY_PARTITION_KERNEL);
+    	result = pspSdkLoadStartModule("flash0:/kd/audiocodec.prx", PSP_MEMORY_PARTITION_KERNEL);
+    	result = pspSdkLoadStartModule("flash0:/kd/mpegbase.prx", PSP_MEMORY_PARTITION_KERNEL);
+    	result = pspSdkLoadStartModule("flash0:/kd/mpeg_vsh.prx", PSP_MEMORY_PARTITION_USER);
+    	pspSdkFixupImports(result);
 #else
 	int result = sceUtilityLoadAvModule(0);
 	result = sceUtilityLoadAvModule(3);
 #endif
-    	sceMpegInit(); 
+    	sceMpegInit();
 }
 
 void CoolAudioDefaultFini()
 {
-	
+
 }
 #endif
