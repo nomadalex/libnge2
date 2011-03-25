a7378014d89cc474614b125d2c4aaca1ed9f8e23
Kun Wang <ifreedom.cn@gmail.com>
2011-03-25 20:09:42 +0800

    Add CMakeLists.txt nge_ch_io_dir.h

---
+++ dirent/nge_ch_io_dir.h
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *            nge_ch_io_dir.h
+ *
+ *  2011/03/25 08:49:34
+ *  Copyright  2011  Kun Wang <ifreedom.cn@gmail.com>
+ ****************************************************************************/
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _NGE_CH_IO_DIR_H
+#define _NGE_CH_IO_DIR_H
+
+#include "nge_dirent.hpp"
+#warning nge_ch_io_dir.h will be deprecated, please use nge_dirent.hpp!
+
+#endif /* _NGE_CH_IO_DIR_H */
