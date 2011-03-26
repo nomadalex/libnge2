7b32637edb35a0689c682a82c3536d497a956f26
Kun Wang <ifreedom.cn@gmail.com>
2011-03-26 11:18:28 +0800

    delete nge_utils.
    Move the functions in nge_utils to nge_misc.

--- include/nge_misc.h
+++ include/nge_misc.h
@@ -19,6 +19,11 @@
 extern "C"{
 #endif
 
+//生成一个min---max的浮点数
+float rand_float(float min, float max);
+//生成一个(min,max)的整数
+int rand_int(int min, int max);
+
 /**
  *将GBK编码的字符转化为unicode字符
  *注意outbuffer需要预先分配足够大小.
