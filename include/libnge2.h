07f26481aee2d4d193653bc0d41f5b1b808698ba
Kun Wang <ifreedom.cn@gmail.com>
2011-04-27 21:14:29 +0800

    Modify libnge2.h
    Rename header nge_graphics_ext.h to nge_image_ext.h
    Rename header nge_gif_animation.h to nge_gif.h

--- include/libnge2.h
+++ include/libnge2.h
@@ -1,19 +1,19 @@
 #ifndef LIBNGE2_H_
 #define LIBNGE2_H_
 
-#include "nge_sys_compatible.h"
 #include "nge_common.h"
 #include "nge_debug_log.h"
+#include "nge_sys_compatible.h"
 #include "nge_io_file.h"
 #include "nge_io_dir.h"
 #include "nge_image.h"
 #include "nge_font.h"
 #include "nge_graphics.h"
-#include "nge_graphics_ext.h"
+#include "nge_image_ext.h"
 #include "nge_input.h"
 #include "nge_main.h"
 #include "nge_misc.h"
 #include "nge_timer.h"
-#include "nge_gif_animation.h"
+#include "nge_gif.h"
 
 #endif
