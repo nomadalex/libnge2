8781e7d94e89f8049d324a3075537569fda7c766
Kun Wang <ifreedom.cn@gmail.com>
2011-07-30 18:41:26 +0800

    Add ANDROID support in nge_debug_log.c

--- src/nge_debug_log.c
+++ src/nge_debug_log.c
@@ -1,18 +1,20 @@
+#ifndef NDEBUG
+
 #include "nge_debug_log.h"
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
+
 #ifdef _PSP
 #include <psploadexec_kernel.h>
 #endif
-
-#ifndef NDEBUG
 
 #if defined(__linux__) || defined(_PSP)
 #define _vsnprintf  vsnprintf
 #endif
 
+#if !defined ANDROID && !defined IPHONES
 FILE *g_logfile = NULL, *g_errorfile = NULL;
 
 /**
@@ -35,13 +37,28 @@ inline void nge_printf (FILE** pFile, const char* filename, const char* pMessage
 	fwrite (Message, strlen (Message), 1, *pFile);
 //	fclose (*pFile); -- notice: I don't close it, because system will close it when quit.
 }
+#endif
+
+#ifdef ANDROID
+#include <android/log.h>
+#define  LOG_TAG "libnge2"
+#define NGE_LOG(msg, argptr) __android_log_vprint(ANDROID_LOG_INFO, LOG_TAG, msg, argptr)
+#define NGE_ERR(msg, argptr) __android_log_vprint(ANDROID_LOG_ERROR, LOG_TAG, msg, argptr)
+#elif defined IPHONES
+// -- fixme
+#define NGE_LOG(msg, argptr)
+#define NGE_ERR(msg, argptr)
+#else
+#define NGE_LOG(msg, argptr) nge_printf(&g_logfile, "neg_log.txt", msg, argptr)
+#define NGE_ERR(msg, argptr) nge_printf(&g_errorfile, "neg_error.txt", msg, argptr)
+#endif
 
 void nge_log(const char* pMessage, ...)
 {
 	va_list	ArgPtr;
 
 	va_start (ArgPtr, pMessage);
-	nge_printf(&g_logfile, "neg_log.txt", pMessage, ArgPtr);
+	NGE_LOG(pMessage, ArgPtr);
 	va_end (ArgPtr);
 }
 
@@ -50,7 +67,7 @@ void nge_error(const char* pMessage, ...)
 	va_list	ArgPtr;
 
 	va_start (ArgPtr, pMessage);
-	nge_printf(&g_errorfile, "neg_error.txt", pMessage, ArgPtr);
+	NGE_ERR(pMessage, ArgPtr);
 	va_end (ArgPtr);
 }
 
