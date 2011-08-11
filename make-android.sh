ea9af12fb8bde1c8e32cabc8dde7ffbf01caef16
Kun Wang <ifreedom.cn@gmail.com>
2011-08-11 22:47:34 +0800

    Add make script for android.

---
+++ make-android.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+dir=`pwd`
+if [ "x$1" = "x" ]; then
+	type=Debug
+else
+	type="$1"
+fi
+
+if [ "x$2" = "x" ]; then
+	target=armeabi
+else
+	target="$2"
+fi
+
+if [ "x$3" != "x" ]; then
+	rm -rf build/android/$type/$target
+fi
+
+if [ ! -d build/android/$type/$target ]; then
+	mkdir -p build/android/$type/$target
+fi
+cd build/android/$type/$target
+cmake -DBUILD_FOR=Android -DCMAKE_BUILD_TYPE=$type -DARM_TARGET=$target $dir
+make
