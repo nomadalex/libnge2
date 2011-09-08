dc434d21aba3075e4f12a6d8be57b62cf4c7158c
Kun Wang <ifreedom.cn@gmail.com>
2011-09-09 00:18:58 +0800

    Move scripts to script dir.

--- down-resources.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### down-resources.sh --- download res.zip and extract it to samples dir.
-
-cd samples/
-rm -f res.zip
-rm -rf res
-wget https://github.com/downloads/ifreedom/libnge2/res.zip --no-check-certificate
-7za x res.zip
