a0469b13f7129612513c588ba011ae735f54b6c1
Kun Wang <ifreedom.cn@gmail.com>
2012-02-21 10:50:23 +0800

    Update scripts.

--- script/down-androiddepends.sh
+++ script/down-androiddepends.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-rm -f AndroidDepends.7z
-rm -rf AndroidDepends
+if [ $1 = "clean" ]; then
+	CLEAN=1
+fi
+
+test_cmd() {
+	for cmd in $@; do
+		which $cmd 1>/dev/null 2>&1
+		if [ $? = 0 ]; then
+			echo $cmd
+			break
+		fi
+	done
+}
+
+_7z_cmd="`test_cmd 7z 7zr 7za`"
+
+(( CLEAN )) && rm -f AndroidDepends.7z
+(( CLEAN )) && rm -rf AndroidDepends
+
 wget https://github.com/downloads/ifreedom/libnge2/AndroidDepends.7z --no-check-certificate
-7za x AndroidDepends.7z
+$_7z_cmd x -y AndroidDepends.7z
--- script/down-resources.sh
+++ script/down-resources.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 ### down-resources.sh --- download res.zip and extract it to samples dir.
 
+if [ $1 = "clean" ]; then
+	CLEAN=1
+fi
+
+test_cmd() {
+	for cmd in $@; do
+		which $cmd 1>/dev/null 2>&1
+		if [ $? = 0 ]; then
+			echo $cmd
+			break
+		fi
+	done
+}
+_7z_cmd="`test_cmd 7z 7zr 7za`"
+
+cd samples
+
+(( CLEAN )) && rm -f res.7z
+(( CLEAN )) && rm -rf res
+
 wget https://github.com/downloads/ifreedom/libnge2/res.7z --no-check-certificate
-mv res.7z samples/
-cd samples/
-7z x -y res.7z
+$_7z_cmd x -y res.7z
