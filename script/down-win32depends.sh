b1eba2cef3fc25c19aa9de6a19a407ff8db21af2
Kun Wang <ifreedom.cn@gmail.com>
2012-02-28 21:23:26 +0800

    Update down-win32depends.sh.
    Move Win32Depends.7z to ge.tt.

--- script/down-win32depends.sh
+++ script/down-win32depends.sh
@@ -19,5 +19,5 @@ _7z_cmd="`test_cmd 7z 7zr 7za`"
 (( CLEAN )) && rm -f Win32Depends.7z
 (( CLEAN )) && rm -rf Win32Depends
 
-wget https://github.com/downloads/ifreedom/libnge2/Win32Depends.7z --no-check-certificate
+wget -O Win32Depends2.7z https://open.ge.tt/1/files/7Th6r3E/0/blob --no-check-certificate
 $_7z_cmd x -y Win32Depends.7z
