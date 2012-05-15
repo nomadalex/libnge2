bcec1c7379f5efa527149897aefa2a898f458db6
johnmave126 <johnmave126@gmail.com>
2012-05-16 07:14:02 +0800

    Add description of the functions and enum in comment.
    Signed-off-by: johnmave126 <johnmave126@gmail.com>

--- include/nge_rbtree.h
+++ include/nge_rbtree.h
@@ -29,10 +29,10 @@ typedef struct {                // value related to key
 /////////////////////////////////////
 
 typedef enum {
-		RBT_STATUS_OK,
-		RBT_STATUS_MEM_EXHAUSTED,
-		RBT_STATUS_DUPLICATE_KEY,
-		RBT_STATUS_KEY_NOT_FOUND
+		RBT_STATUS_OK, /** 成功*/
+		RBT_STATUS_MEM_EXHAUSTED,/** 空间不足*/
+		RBT_STATUS_DUPLICATE_KEY,/** 索引重复*/
+		RBT_STATUS_KEY_NOT_FOUND/** 索引无法找到*/
 } RbtStatus;
 
 typedef enum { BLACK, RED } nodeColor;
@@ -56,12 +56,57 @@ typedef void (*travelcall)(rbtree_p rb,NodeType *);
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/**
+ *向一棵红黑树中插入一个key, val对
+ *@param[IN, OUT] rb,要插入的红黑树
+ *@param[IN] key,标识值的索引
+ *@param[IN] val,要插入的值
+ *@return RbtStatus,操作成功即为RBT_STATUS_OK，否则请参考RbtStatus定义
+ */
 	NGE_API RbtStatus rbtInsert(rbtree_p rb,KeyType key, ValType val);
+
+/**
+ *从一棵红黑树中删除一个节点
+ *@param[IN, OUT] rb,要删除的红黑树
+ *@param[IN] z,待删除节点
+ *@return RbtStatus,操作成功即为RBT_STATUS_OK，否则请参考RbtStatus定义
+ */
 	NGE_API RbtStatus rbtErase(rbtree_p rb,NodeType * z);
+
+/**
+ *从一棵红黑树中找到一个索引对应的节点
+ *@param[IN] rb,要搜索的红黑树
+ *@param[IN] key,待搜索索引
+ *@return NodeType*,搜索到的节点,若搜索失败,返回NULL
+ */
 	NGE_API NodeType* rbtFind(rbtree_p rb,KeyType key);
+
+/**
+ *从一棵红黑树中某个节点开始进行先序遍历
+ *@param[IN] rb,要遍历的红黑树
+ *@param[IN] p,要开始的节点
+ *@param[IN] callback,回调函数,对每一个节点进行调用
+ */
 	NGE_API void rbtInorder(rbtree_p rb,NodeType *p, travelcall callback);
+
+/**
+ *从一棵红黑树中删除某个节点的整个子树
+ *@param[IN] rb,要删除的红黑树
+ *@param[IN] z,开始的节点
+ */
 	NGE_API void rbtDelete(rbtree_p rb,NodeType *p);
+
+/**
+ *创建一棵红黑树
+ *@return rbtree_p,创建的红黑树
+ */
 	NGE_API rbtree_p createTree();
+
+/**
+ *销毁一棵红黑树
+ *@param[IN] rb,要销毁的红黑树
+*/
 	NGE_API void destoryTree(rbtree_p rb);
 #ifdef __cplusplus
 }
