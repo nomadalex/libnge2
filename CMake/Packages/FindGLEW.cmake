b92e8fe02cfe404dd9309d6868983dae200decc6
JohnMave126 <johnmave126@gmail.com>
2012-08-18 11:37:24 +0800

    Update cmake to support glew.
    Signed-off-by: JohnMave126 <johnmave126@gmail.com>

---
+++ CMake/Packages/FindGLEW.cmake
@@ -0,0 +1,20 @@
+#
+# Try to find GLEW library and include path.
+# Once done this will define
+#
+# GLEW_FOUND
+# GLEW_INCLUDE_PATH
+# GLEW_LIBRARY
+# 
+
+if(NOT GLEW_FOUND)
+  include(PkgUtils)
+  findpkg_begin(GLEW)
+
+  define_find_pkg(GLEW
+	PKG_CONFIG_NAME glew
+	LIBRARY_HINTS glew GLEW glew32 glew32s
+	HEADER_HINTS gl/glew.h GL/glew.h)
+	
+  findpkg_finish(GLEW)
+endif()
