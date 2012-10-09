a825d4a3ce0617ec4f65f5c45ea950798f72f63a
JohnMave126 <johnmave126@gmail.com>
2012-10-09 16:08:43 +0800

    Force dontswizzle to rendertarget.
    Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- include/nge_graphics.h
+++ include/nge_graphics.h
@@ -121,7 +121,6 @@ extern "C"{
  * 绘制到图片开始函数,所有绘制到图片均在BeginTarget,EndTarget之间调用
  *@param[in] _img 所要绘制到的图片
  *@return 无
- *@warning 如果所要使用的图片需要多次进行绘制，请务必将dontswizzle属性设置为1
  */
 	NGE_API BOOL BeginTarget(image_p _img);
 /**
