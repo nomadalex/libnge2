956ab220e61809f467fd66a5d12b1b678d017b11
Kun Wang <ifreedom@ifreedom-virtual-machine.(none)>
2012-10-29 22:33:23 +0800

    Add android jni base.
    So all jni calls can use same javaVM, and can compile into one dll/so.

---
+++ and_jni_base/nge_android_jni.h
@@ -0,0 +1,23 @@
+/**
+ * @file  nge_android_jni.h
+ * @author Kun Wang <ifreedom.cn@gmail.com>
+ * @date 2012/10/21 14:46:24
+ *
+ *  Copyright  2012  Kun Wang <ifreedom.cn@gmail.com>
+ *
+ */
+
+#ifndef _NGE_ANDROID_JNI_H
+#define _NGE_ANDROID_JNI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	JNIEnv* nge_GetEnv();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _NGE_ANDROID_JNI_H */
